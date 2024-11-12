f067b69fdd7bb3fb9965dd441abb3e18475693ad
jfbricenof81 <julianbricenoflorezsena@gmail.com>
2024-11-12 09:11:36 -0500

    aaa

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    public function hola()
+    {
+        return 'Este es un mensaje';
+    }
+}
